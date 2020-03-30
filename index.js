501a93e54fe73ce9c1922ba91db0cfcee9358b3a
osgood gunawan <ogunawan@ucsd.edu>
2020-03-30 18:28:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(driver){
+
+return driver.slice(0,2)    
+}
+
+const returnLastTwoDrivers= function(driver){
+    return driver.slice(-2)
+}
+
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers ]
+
+
+const createFareMultiplier=function (multiply){
+    return function(fare){
+        return multiply * fare
+    }
+}
+
+const fareDoubler=createFareMultiplier(2);
+
+const fareTripler=createFareMultiplier(3);
+
+
+function selectDifferentDrivers(arrayOfDrivers,driverfunction){
+    return driverfunction(arrayOfDrivers);
+}
+
+
+
